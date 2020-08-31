a12555387d72ed44a42d4b32245f7e75f72a7c36
Attila Szücs <ati.szucs@hotmail.com>
2020-08-31 14:58:09 +0200

    show loading indicator when listing blogposts

--- RobotBlog/ClientApp/src/pages/blog/blog.js
+++ RobotBlog/ClientApp/src/pages/blog/blog.js
@@ -5,43 +5,54 @@ import { useTranslation } from 'react-i18next';
 import blogService from '../../services/blog/blog.service';
 import Post from './components/Post';
 import './blog.css';
+import Loading from '../../components/loading/loading';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
   const { i18n } = useTranslation();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const loadBlogPosts = async () => {
+      setLoading(true);
       setPosts(await blogService.loadBlogPosts());
+      setLoading(false);
     };
 
     loadBlogPosts();
   }, []);
 
+  const PostList = () => {
+    const components = posts.map((post) => {
+      const translations = post.translatedBlogPosts;
+      // eslint-disable-next-line
+      // TODO: filter on server side? or do not filter at all => show empty post with "This content is not available in this language?"
+      const currentBlogs = translations.filter((item) => item.language === i18n.language);
+      return currentBlogs.map((translated) => {
+        const firstParagraphTranslated = {
+          ...translated,
+          content: translated.content.split(/\r?\n/)[0],
+        };
+
+        const current = {
+          blogPostId: post.blogPostId,
+          date: post.date,
+          user: post.user,
+          translated: firstParagraphTranslated,
+        };
+
+        return <Post currentPost={current} key={uuid()} />;
+      });
+    });
+    return (components);
+  };
+
+  const toDisplay = loading ? <Loading /> : <PostList />;
+
   return (
     (
       <div>
-        {posts.map((post) => {
-          const translations = post.translatedBlogPosts;
-          // eslint-disable-next-line
-          // TODO: filter on server side? or do not filter at all => show empty post with "This content is not available in this language?"
-          const currentBlogs = translations.filter((item) => item.language === i18n.language);
-          return currentBlogs.map((translated) => {
-            const firstParagraphTranslated = {
-              ...translated,
-              content: translated.content.split(/\r?\n/)[0],
-            };
-
-            const current = {
-              blogPostId: post.blogPostId,
-              date: post.date,
-              user: post.user,
-              translated: firstParagraphTranslated,
-            };
-
-            return <Post currentPost={current} key={uuid()} />;
-          });
-        })}
+        {toDisplay}
       </div>
     )
   );
