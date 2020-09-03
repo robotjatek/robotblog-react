96e8530f9634ae11d13ce2a255ca0938295714ce
Attila Szücs <ati.szucs@hotmail.com>
2020-09-04 00:24:37 +0200

    Validate PreferredLanguage and username in DTO

---
+++ RobotBlog/Controllers/DTOs/Validators/StringRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace RobotBlog.Controllers.DTOs.Validators
+{
+    public class StringRangeAttribute : ValidationAttribute
+    {
+        public string[] ValidValues { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && ValidValues.Contains(value.ToString().ToLowerInvariant()))
+            {
+                return ValidationResult.Success;
+            }
+            var member = validationContext.DisplayName;
+            var valid = string.Join(", ", ValidValues);
+            return new ValidationResult($"{member} must be one of these values: {valid}");
+        }
+    }
+}
